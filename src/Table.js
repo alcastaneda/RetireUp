fdd43db50a443465bc5523bf882bb0fc2b052c49
Adriana <adriana.lcs316@gmail.com>
2020-08-15 22:50:07 -0500

    add table component

---
+++ src/Table.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Table({ data, min }) {
+  return (
+    <table className="Table">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Return</th>
+          <th>Cumulative Return</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+  );
+}
